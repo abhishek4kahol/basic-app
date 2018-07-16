fcb802f2043d0abda1109d30631631a2629591d3
abhishek_kahol <intern.abhishek.kahol@firstcry.com>
2018-07-16 18:47:36 +0530

    nav bar model update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,4 @@
-<<template>
+<template>
   <section>
     <div>
       <nav class="navbar is-transparent is-warning is-fixed-top has-shadow">
@@ -38,7 +38,6 @@
                       </div>
                     </div>
                   </div>
-                </p>
              </div>
            </div>
           </div>
@@ -56,7 +55,7 @@
          </div> -->
         <div class="hero-body">
          <div class="container">
-                       <h1 class="title is-1 set-margin">
+            <h1 class="title is-1 set-margin">
               Welcome to Trip-Stamp
             </h1>
            <div class="field is-horizontal">
@@ -88,11 +87,11 @@
           Explore Different LandForms
         </h1>
         <div class="columns is-multiline is-mobile is-centered">
-        <a class="button is-primary icon is-small is-left has-text-warning" style="display:block; margin-right:auto; height:229px; width: 41px; background-color: #F1ECE7; border-radius: 25px;">
-          <i class="fas fa-2x	fa-arrow-left" style="padding-top: 92px;"></i>
+        <a class="button icon is-small is-left has-text-warning is-outlined" style="display:block; margin-right:auto; height:229px; width: 41px; background-color: #F1ECE7; border-radius: 25px;">
+          <i class="fas fa-2x	fa-arrow-left has-text-warning has-shadow is-bold" style="padding-top: 92px;"></i>
         </a>
-          <div class="column" style="min-width: 150px; max-width: 228px;">
-            <div class="card" style="min-width: 150px; max-width: 228px;">
+          <div class="column " style="min-width: 150px; max-width: 228px;">
+            <div class="card set-thumbnail" style="min-width: 150px; max-width: 228px;" v-on:click="getInfo()">
               <div class="card-image">
                 <figure class="image tooltip">
                   <img src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
@@ -141,21 +140,55 @@
               </div>
             </div>
           </div>
-          <a class="button is-primary icon is-small is-left has-text-warning" style="display:block; margin-left:auto;height:229px; width: 41px; background-color: #F1ECE7; border-radius: 25px;">
+          <a class="button icon is-small is-left has-text-warning is-outlined" style="display:block; margin-left:auto;height:229px; width: 41px; background-color: #F1ECE7; border-radius: 25px;">
             <i class="fas fa-2x	fa-arrow-right" style="padding-top: 92px;"></i>
           </a>
         </div>
     </div>
   </div>
   </section>
-  <hr>
-  <section class="hero is-info is-bold">
+  <section class="hero is-info">
+    <div class="hero-head">
+      <div class="container">
+        <br>
+        <h1 class="title">
+        <span class="icon is-medium is-info">
+          <i class="fas fa-map-marked"></i>
+        </span>
+          Create Your Stamp In
+        </h1>
+      </div>
+    </div>
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">
-         Create Your List From
-        </h1>
-
+        <div>
+          <nav class="level is-mobile">
+            <div class="level-item has-text-centered">
+              <div>
+                <p class="heading">HoT SPOT</p>
+                <p class="title">1000+</p>
+              </div>
+            </div>
+            <div class="level-item has-text-centered">
+              <div>
+                 <p class="heading">Union-territories</p>
+                 <p class="title">7</p>
+              </div>
+            </div>
+            <div class="level-item has-text-centered">
+              <div>
+                <p class="heading">Other</p>
+                <p class="title">29</p>
+              </div>
+            </div>
+            <div class="level-item has-text-centered">
+              <div>
+                <p class="heading">new</p>
+                <p class="title">29</p>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
   </section>
@@ -231,7 +264,6 @@
       <div class="content has-text-centered">
         <p>
           <strong>Trip-Stamp</strong> by <a href="https://github.com/abhishek4kahol">Abhishek</a>.
-        
         </p>
       </div>
   </footer>
@@ -239,73 +271,74 @@
   </section>
 </template>
 <script>
-export default {
-  name: 'Home',
-  data () {
-    return {
+  export default {
+    name: "home",
+    data() {
+      return {
 
+      }
+    },
+    methods: {
+      getInfo: function () {
+        console.log('click success');
+        this.$router.push({
+          name: 'PlaceDescription'
+          // query: {
+          //   data: TransObj
+          // }
+        })
+      }
     }
   }
-}
 </script>
-
 <style>
-  .bg-img { 
-        background-image: url("https://static.tacdn.com/img2/branding/homepage/home-tab1-hero-1367x520-beach-prog.jpg") ;
-        background-position: 50% bottom;
-        background-repeat:  no-repeat;
-        /* background-attachment: fixed; */
-        /* background-size:  cover; */
-        background-color: #999;
- }
- .set-width {
-   width:70% !important;
- }
- .set-margin {
-   font-family: 'Gaegu', cursive;
-   /* margin-top: 8px; */
- }
- .set-thumbnail {
-    border: 1px solid #ddd;
-    border-radius: 2px;
-    padding: 1px;
-    /* background-color: #cbefd5; */
+.bg-img {
+  background-image: url("https://static.tacdn.com/img2/branding/homepage/home-tab1-hero-1367x520-beach-prog.jpg");
+  background-position: 50% bottom;
+  background-repeat: no-repeat;
+  background-color: #999;
+}
+.set-width {
+  width: 70% !important;
+}
+.set-margin {
+  font-family: "Gaegu", cursive;
+}
+.set-thumbnail {
+  padding: 1px;
 }
 .set-thumbnail:hover {
-    /* box-shadow: 0 0 2px 1px rgba(165, 222, 241, 0.5); */
-    cursor: pointer;
-    opacity: 0.7;
+  cursor: pointer;
+  opacity: 0.9;
 }
 .tooltip {
-    position: relative;
-    display: inline-block;
-    border-bottom: 1px dotted #00DCB5;
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted #00dcb5;
 }
 .tooltip .tooltiptext {
-    visibility: hidden;
-    width: 120px;
-    background-color: #00DCB5;
-    color: #fff;
-    text-align: center;
-    border-radius: 6px;
-    /* padding: 5px 0; */
-    position: absolute;
-    z-index: 1;
-    bottom: 102%;
-    left: 50%;
-    margin-left: -60px;
+  visibility: hidden;
+  width: 120px;
+  background-color: #00dcb5;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  position: absolute;
+  z-index: 1;
+  bottom: 102%;
+  left: 50%;
+  margin-left: -60px;
 }
 .tooltip .tooltiptext::after {
-    content: "";
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    margin-left: -5px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: #00DCB5 transparent transparent transparent;
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #00dcb5 transparent transparent transparent;
 }
 .tooltip:hover .tooltiptext {
-    visibility: visible;
+  visibility: visible;
 }
 </style>
